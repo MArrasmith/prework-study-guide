6d9c859c8817061d7357cb0044513968dec2e6fd
Michael Arrasmith <michael.keith.arrasmith@gmail.com>
2023-08-08 22:09:22 -0400

    added js file

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+/*
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+*/
